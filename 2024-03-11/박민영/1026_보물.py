3b9eaefc9f7de1387723cc913673ac167d80fff0
jellimin <98722620+jellimin@users.noreply.github.com>
2024-03-06 00:10:02 +0900

    240306

---
+++ 2024-03-11/박민영/1026_보물.py
@@ -0,0 +1,18 @@
+# 배열 길이 N, 배열 A, B
+# S = A[0] × B[0] + ... + A[N-1] × B[N-1]의 최솟값
+# 단, B의 수는 재배열하면 안 됨
+
+import sys
+
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+S = 0
+
+A.sort()
+
+for i in A:
+    S += i * max(B)
+    B.pop(B.index(max(B)))
+
+print(S)
