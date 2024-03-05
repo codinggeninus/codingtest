3b9776ccdc61c3f27ba1492f1587721a4e01e59c
jellimin <98722620+jellimin@users.noreply.github.com>
2024-03-05 11:43:42 +0900

    20240305_commit

---
+++ 2024-03-11/박민영/11047_동전 0.py
@@ -0,0 +1,18 @@
+# 동전 N종류, 동전 가치합 K
+# 필요한 동전 개수의 최소값
+
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+coin = [int(sys.stdin.readline()) for _ in range(N)] # 동전 N종류 가치 오름차순
+coin.sort(reverse = True)
+cnt = 0
+
+for i in coin:
+    # 정지조건
+    if K == 0:
+        break
+    cnt += K // i # 동전 개수 카운트
+    K %= i # 동전 가치합 업데이트
+
+print(cnt)
