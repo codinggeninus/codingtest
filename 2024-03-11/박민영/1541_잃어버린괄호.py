a9aec2398c08cb76b868e6c722a425b725d5ab1f
jellimin <98722620+jellimin@users.noreply.github.com>
2024-03-11 00:02:13 +0900

    Add 11501_주식.py, 1541_잃어버린괄호.py

---
+++ 2024-03-11/박민영/1541_잃어버린괄호.py
@@ -0,0 +1,10 @@
+import sys
+
+formu = sys.stdin.readline().strip()
+formu = formu.split('-') # '-'보다 '+' 연산을 먼저 해주어야 함
+res = sum(map(int, formu[0].split('+'))) # 연산식의 첫 값 저장
+
+for i in formu[1:]:
+    res -= sum(map(int, i.split('+')))
+
+print(res)
