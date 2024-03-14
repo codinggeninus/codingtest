345aac2e7b8c2064bda4312365ab4b1ce64626f0
sinkyoungdeok <tkagmd1@naver.com>
2024-03-15 00:37:49 +0900

    add 15650

--- 2024-03-18/신경덕/15650_N과M(2).py
+++ 2024-03-18/신경덕/15650_N과M(2).py
@@ -1,22 +1,16 @@
-# 백트래킹, 실버3
-# https://www.acmicpc.net/problem/
-# 1. n, m을 for 문을 돌면서 재귀 실행 (백트래킹) 중복x
-# 2. n! < 8
-# 3. answer[]
-n, m = map(int, input().split())
-answer = []
+N, M = map(int, input().split())
+temp = []
 
 
-def dfs(start):
-    if len(answer) == m:
-        print(' '.join(map(str, answer)))
-        return
+def dfs(i):
+  if M == len(temp):
+    print(' '.join(map(str, temp)))
+    return
 
-    for i in range(start, n+1):
-        if i not in answer:
-            answer.append(i)
-            dfs(i+1)
-            answer.pop()
+  for j in range(i, N + 1):
+    temp.append(j)
+    dfs(j + 1)
+    temp.pop()
 
 
 dfs(1)
