11c34fb32d155ebeca58763168be108b711c6861
sinkyoungdeok <tkagmd1@naver.com>
2024-03-17 14:41:46 +0900

    add 15654

--- 2024-03-18/신경덕/15654_N과M(5).py
+++ 2024-03-18/신경덕/15654_N과M(5).py
@@ -1,24 +1,19 @@
-# 백트래킹, 실버3
-# https://www.acmicpc.net/problem/15654
-# 1. 백트래킹, n개의 수가 주어짐, 중복x, 
-# 2. n^n <= 8
-# 3. answer[]
-
-n, m = map(int, input().split())
-n_list = list(map(int, input().split()))
-n_list.sort()
-answer = []
+N, M = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+temp = []
 
 
-def backtracking(start):
-    if len(answer) == m:
-        print(' '.join(map(str, answer)))
+def dfs():
+    if M == len(temp):
+        print(' '.join(map(str, temp)))
         return
-    for i in range(n):
-        if n_list[i] not in answer:
-            answer.append(n_list[i])
-            backtracking(start+1)
-            answer.pop()
+
+    for j in range(1, N + 1):
+        if nums[j-1] in temp:
+            continue
+        temp.append(nums[j-1])
+        dfs()
+        temp.pop()
 
 
-backtracking(0)
+dfs()
