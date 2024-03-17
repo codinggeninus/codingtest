61982b7af9306cff1f51ad2d20590749257fc562
sinkyoungdeok <tkagmd1@naver.com>
2024-03-17 14:49:05 +0900

    add 15655

--- 2024-03-18/신경덕/15655_N과M(6).py
+++ 2024-03-18/신경덕/15655_N과M(6).py
@@ -1,24 +1,17 @@
-# 백트래킹, 실버3
-# https://www.acmicpc.net/problem/15655
-# 1. 백트래킹, n개의 수가 주어짐, 각 수열은 오름차순, 중복x
-# 2. n!
-# 3. answer[]
-
-n, m = map(int, input().split())
-n_list = list(map(int, input().split()))
-n_list.sort()
-answer = []
+N, M = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+temp = []
 
 
-def backtracking(start):
-    if len(answer) == m:
-        print(' '.join(map(str, answer)))
+def dfs(i):
+    if M == len(temp):
+        print(' '.join(map(str, temp)))
         return
-    for i in range(start, n):
-        if n_list[i] not in answer:
-            answer.append(n_list[i])
-            backtracking(i+1)
-            answer.pop()
+
+    for j in range(i, N + 1):
+        temp.append(nums[j-1])
+        dfs(j + 1)
+        temp.pop()
 
 
-backtracking(0)
+dfs(1)
