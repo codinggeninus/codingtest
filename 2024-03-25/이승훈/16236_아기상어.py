31c0ce975a53714950808970560c4c98a3f61c2a
LEE-sh1673 <lsh901673@gmail.com>
2024-03-22 19:41:33 +0900

    refactor: 아기 상어(16236) 메서드 유형 변경
    - find_fish 메서드를 내부 함수에서 외부 함수로 변경

--- 2024-03-25/이승훈/16236_아기상어.py
+++ 2024-03-25/이승훈/16236_아기상어.py
@@ -49,52 +49,50 @@ def find_shark():
                 return Shark(i, j)
 
 
-def solution(n, space, shark):
-    def find_fish():
-        """
-        bfs 탐색을 통해 먹을 수 있는 물고기를 탐색합니다.
-        조건에 만족하는 물고기가 1개 이상 존재할 경우
-        (1.거리 -> 2.위 -> 3.왼쪽)을 기준으로 오름차 순 정렬 후 첫 번째 요소를 반환합니다.
-        """
-        fishes = []
-        queue = deque()
-        visited = [[False] * n for _ in range(n)]
+def find_fish():
+    """
+    bfs 탐색을 통해 먹을 수 있는 물고기를 탐색합니다.
+    조건에 만족하는 물고기가 1개 이상 존재할 경우
+    (1.거리 -> 2.위 -> 3.왼쪽)을 기준으로 오름차 순 정렬 후 첫 번째 요소를 반환합니다.
+    """
+    fishes = []
+    queue = deque()
+    visited = [[False] * n for _ in range(n)]
 
-        queue.append((shark.x, shark.y, 0))
-        visited[shark.x][shark.y] = True
+    queue.append((shark.x, shark.y, 0))
+    visited[shark.x][shark.y] = True
 
-        while queue:
-            i, j, cnt = queue.popleft()
+    while queue:
+        i, j, cnt = queue.popleft()
 
-            for dir_x, dir_y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                x = i + dir_x
-                y = j + dir_y
+        for dir_x, dir_y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            x = i + dir_x
+            y = j + dir_y
 
-                if x < 0 or x > n - 1 or y < 0 or y > n - 1:
-                    continue
+            if x < 0 or x > n - 1 or y < 0 or y > n - 1:
+                continue
 
-                if not visited[x][y] and space[x][y] <= shark.size:
-                    visited[x][y] = True
-                    queue.append((x, y, cnt + 1))
+            if not visited[x][y] and space[x][y] <= shark.size:
+                visited[x][y] = True
+                queue.append((x, y, cnt + 1))
 
-                    if 0 < space[x][y] < shark.size:
-                        fishes.append(Fish(x, y, cnt + 1))
+                if 0 < space[x][y] < shark.size:
+                    fishes.append(Fish(x, y, cnt + 1))
 
-        if not fishes:
-            return []
+    if not fishes:
+        return []
 
-        fish = sorted(fishes, key=lambda fish: (fish.dist, fish.x, fish.y))[0]
-        space[shark.x][shark.y] = 0
-        space[fish.x][fish.y] = 0
-        return fish
-
-    while fish := find_fish():
-        shark.eat(fish)
-
-    print(shark.dist)
+    fish = sorted(fishes, key=lambda fish: (fish.dist, fish.x, fish.y))[0]
+    space[shark.x][shark.y] = 0
+    space[fish.x][fish.y] = 0
+    return fish
 
 
 n = int(input())
 space = [list(map(int, input().strip().split())) for _ in range(n)]
 shark = find_shark()
-solution(n, space, shark)
+
+while fish := find_fish():
+    shark.eat(fish)
+
+print(shark.dist)
