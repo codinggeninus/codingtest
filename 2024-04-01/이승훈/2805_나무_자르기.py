ada5e84e796fcd463599479f4bee24981383aa32
SeungHoon Lee <lsh901673@gmail.com>
2024-04-07 20:07:37 +0900

    solve: 나무 자르기(2805) 풀이

---
+++ 2024-04-01/이승훈/2805_나무_자르기.py
@@ -0,0 +1,22 @@
+"""
+2805. 나무 자르기
+"""
+from sys import stdin
+
+input = stdin.readline
+n, m = map(int, input().split())
+trees = list(map(int, input().split()))
+
+
+def search(left, right):
+    while left < right:
+        mid = left + (right - left) // 2
+        total = sum([tree - mid for tree in trees if tree > mid])
+        
+        if total < m:
+            right = mid
+        else:
+            left = mid + 1
+    return left - 1
+
+print(search(0, max(trees)))
