44ad60e5cf10638b8a75f26aeea0a3c939ea2cff
LEE-sh1673 <lsh901673@gmail.com>
2024-04-08 21:11:51 +0900

    solve: 이분 탐색 풀이 수정
    - 용액 (2467)
    - 나무 자르기 (2805)
    - 숫자 카드 2 (10816)

---
+++ 2024-04-08/이승훈/2467_용액.py
@@ -0,0 +1,32 @@
+"""
+2467. 용액
+"""
+from sys import stdin
+
+input = stdin.readline
+n = int(input())
+juices = list(map(int, input().split()))
+min_blending = abs(juices[0] + juices[-1])
+
+
+def search(start, end):
+    ans_x, ans_y = 0, n - 1
+    min_blending = abs(juices[start] + juices[end])
+
+    while start < end:
+        blending = juices[start] + juices[end]
+
+        if min_blending > abs(blending):
+            min_blending = abs(blending)
+            ans_x, ans_y = start, end
+
+        if blending < 0:
+            start += 1
+        else:
+            end -= 1
+
+    return ans_x, ans_y
+
+
+ans_x, ans_y = search(0, n - 1)
+print(juices[ans_x], juices[ans_y])
